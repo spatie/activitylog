0952b5776c4d080eb92c8759982bc264f5632107
MatthiasDeWinter <dewinter.matthias@gmail.com>
2015-04-01 07:29:18 +0000

    add timestamps to migration

--- src/migrations/create_activity_log_table.php
+++ src/migrations/create_activity_log_table.php
@@ -14,9 +14,8 @@ class CreateActivityLogTable extends Migration
             $table->increments('id');
             $table->integer('user_id')->nullable();
             $table->string('text');
-            $table->timestamp('created_at');
             $table->string('ip_address', 64);
-
+            $table->timestamps();
         });
     }
 
